42470c8d9ae6725737577cc91557f71a7385e9cc
Emine Elif Toscali <etoscali@gmail.com>
2014-02-02 19:10:38 +0200

    birthdate chooser works properly

--- src/com/example/androidasitakvimi/DatePickerFragment.java
+++ src/com/example/androidasitakvimi/DatePickerFragment.java
@@ -1,5 +1,6 @@
 package com.example.androidasitakvimi;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 import android.app.DatePickerDialog;
@@ -10,31 +11,41 @@ import android.widget.DatePicker;
 
 public class DatePickerFragment extends DialogFragment
 implements DatePickerDialog.OnDateSetListener {
+	 TheListener listener;
+
+	 public interface TheListener{
+	     public void returnDate(String date);
+	 }
+
 
 @Override
 public Dialog onCreateDialog(Bundle savedInstanceState) {
 // Use the current date as the default date in the picker
-final Calendar c = Calendar.getInstance();
-int year = c.get(Calendar.YEAR);
-int month = c.get(Calendar.MONTH);
-int day = c.get(Calendar.DAY_OF_MONTH);
-
+	final Calendar c = Calendar.getInstance();
+	int year = c.get(Calendar.YEAR);
+	int month = c.get(Calendar.MONTH);
+	int day = c.get(Calendar.DAY_OF_MONTH);
+	listener = (TheListener) getActivity(); 
 
 // Create a new instance of DatePickerDialog and return it
 return new DatePickerDialog(getActivity(), this, year, month, day);
 }
 
-public String onDateSet(int year, int month, int day) {
+public void onDateSet(DatePicker view,int year, int month, int day) {
     
 	
-	return null;
-// Do something with the date chosen by the user
-	
+	Calendar c = Calendar.getInstance();
+	c.set(year, month, day);
+
+	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+	String formattedDate = sdf.format(c.getTime());
+	if (listener != null) 
+	{
+	  listener.returnDate(formattedDate); 
+
+	}
+
 }
 
-@Override
-public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3) {
-	// TODO Auto-generated method stub
-	
-}
+
 }
--- src/com/example/androidasitakvimi/MainActivity.java
+++ src/com/example/androidasitakvimi/MainActivity.java
@@ -9,56 +9,47 @@ import android.app.DatePickerDialog.OnDateSetListener;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.FragmentManager;
+import android.text.format.DateFormat;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements DatePickerFragment.TheListener {
 	TextView mgetBirthdateText;
-	DatePicker mgetBirthDate;
+	//DatePicker mgetBirthDate;
 	Button mgetBirthDateButton;
-	final Calendar c = Calendar.getInstance();
-	int year = c.get(Calendar.YEAR);
-	int month = c.get(Calendar.MONTH);
-	int day = c.get(Calendar.DAY_OF_MONTH);
+	
+	
+
+	//java.text.DateFormat fmtDateAndTime = DateFormat.getDateTimeInstance();
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		mgetBirthDate = (DatePicker) findViewById(R.id.datePicker1);
+		//mgetBirthDate = (DatePicker) findViewById(R.id.datePicker1);
 		mgetBirthdateText = (TextView) findViewById(R.id.textView1);
-		
-		
 		mgetBirthDateButton = (Button) findViewById(R.id.buttonhesapla);
 		mgetBirthDateButton.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				showDateText(year, month, day);
+				DialogFragment picker = new DatePickerFragment();
+                picker.show(getFragmentManager(), "datePicker");
+                
+
 			}
 		});
+		
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+	@Override 
+    public void returnDate(String date) {
+        // TODO Auto-generated method stub
+        mgetBirthdateText.setText(date);
+    }
 	
     
-    public void showDatePickerDialog(View v) {
-    	
-    	    DialogFragment newFragment = new DatePickerFragment();
-    	    newFragment.show(getFragmentManager(), "datePicker");
-    	
-    }
-    public void showDateText(int year,int month,int day){
-    	
-    	mgetBirthdateText.setText(String.valueOf(year)+String.valueOf(month)+String.valueOf(day));
-    }
 }
 
